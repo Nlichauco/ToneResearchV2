085eccea317036862b25c0b0bed7f9bdd7b8023c
Folaido <65743027+Folaido@users.noreply.github.com>
2021-05-17 18:29:51 -0500

    Update moreclean.py

--- moreclean.py
+++ moreclean.py
@@ -6,105 +6,13 @@ from natsort import natsorted
 import requests
 from bs4 import BeautifulSoup
 
-
-def Get_cutoff(path):
-    sum = []
-
-    missing_values = ["NaN", "0"]
-    # print(os.listdir(path))
-
-    for filename in natsorted(os.listdir(path), key=lambda y: y.lower()):
-
-        if not filename.startswith('.'):
-            # print(filename,"\n")
-
-            with open(os.path.join(path, filename)) as f:
-
-                numset = set()
-
-                data = pd.read_csv(f, header=0, sep=r'\s*,\s*', na_values=missing_values, engine='python',
-                                   usecols=[0, 1, 2, 5, 6, 7, 8, 9, 10, 11])
-                length = len(data.index)
-
-                if length <= 10:
-                    for i in range(0, length):
-                        articlelength = get_text(data['URL'][i])
-                        sum.append(articlelength)
-                else:
-                    i = 0
-
-                    while (i < 10):
-
-                        number = random.randrange(0, length)
-
-                        if number not in numset:
-                            numset.add(number)
-                            articlelength = get_text(data['URL'][number])
-                            sum.append(articlelength)
-                            i += 1
-    sum.sort()
-    listLen = len(sum)
-    cutoff = int(.05 * listLen)
-
-    return sum[cutoff]
-
-
-def Get_cut(path):
-    sum = 0
-    dupe = 0
-    outfile = open("out3.txt", "a")
-
-    missing_values = ["NaN", "0"]
-    # print(os.listdir(path))
-
-    for filename in natsorted(os.listdir(path), key=lambda y: y.lower()):
-
-        if not filename.startswith('.'):
-            # print(filename,"\n")
-
-            with open(os.path.join(path, filename)) as f:
-                numset = set()
-
-                data = pd.read_csv(f, header=0, sep=r'\s*,\s*', na_values=missing_values, engine='python',
-                                   usecols=[0, 1, 2, 5, 6, 7, 8, 9, 10, 11])
-                length = len(data.index)
-                for i in range(0, length):
-                    articlelength = get_text(data['URL'][i])
-                    if articlelength < 500 and data['URL'][i] not in numset:
-                        print("made it")
-                        outfile.write(data['URL'][i] + "\n")
-                        sum += 1
-                        numset.add(data['URL'][i])
-                    elif data['URL'][i] in numset:
-                        dupe += 1
-
-    return sum, dupe
-
-
-def get_text(url):
-    session = requests.Session()
-    text = 0
-    req = session.get(url)
-    soup = BeautifulSoup(req.text, 'html.parser')
-    paragraphs = soup.find_all('p')
-    for p in paragraphs:
-        words = p.get_text()
-        text += len(words)
-    return text
-
-
-def count_effective(file):
-    count = 0
-    count_total = 0
-    with open(file, 'r') as f:
-        for line in f:
-            if clean(line):
-                continue
-            count_total += 1
-            if "picture" in line or "photo" in line or "movie" in line or "music" in line or "book" in line or "share" in line:
-                count += 1
-    print(count_total)
-    print(count)
+"""Count how many articles we have filtered in phrase one and phrase two
+    Args:
+        path: The path of the raw data
+    Returns:
+         No value returned, but print the total number of articles, number of articles filtered in phrase 1, and number 
+         of articles filtered in phrase 2
+    """
 
 
 def count_percentage(path):
@@ -132,17 +40,40 @@ def count_percentage(path):
     print(count_phrase2)
 
 
-def clean(url):
+"""Decide whether given article will be filtered in Phrase 1 filtering
+    Args:
+        url: The url of the article
+    Returns:
+         True if the article will be filtered, False otherwise
+    """
+
+
+def phrase1(url):
     return ('coronavirus' not in url and 'covid' not in url and 'virus' not in url and 'pandemic' not in url
             and 'vaccine' not in url)
 
 
-def NYTclean(url):
-    return (
-                       'coronavirus' not in url and 'covid' not in url and 'virus' not in url and 'pandemic' not in url and 'vaccine' not in url) or 'interactive' in url or 'photo' in url or 'video' in url or "podcast" in url or "quotation-of-the-day" in url
+"""Decide whether given NYT article will be filtered in Phrase 2 filtering
+    Args:
+        url: The url of the article
+    Returns:
+         True if the article will be filtered, False otherwise
+    """
 
 
-def phrase2(url):
+def NYTPhrase2(url):
+    return 'interactive' in url or 'photo' in url or 'video' in url or "podcast" in url or "quotation-of-the-day" in url
+
+
+"""Decide whether given Guardian article will be filtered in Phrase 2 filtering
+    Args:
+        url: The url of the article
+    Returns:
+         True if the article will be filtered, False otherwise
+    """
+
+
+def GuardianPhrase2(url):
     return 'interactive' in url or "picture" in url or "photo" in url or "video" in url \
            or "music" in url or "book" in url or "share" in url
 
