e825908968e9f7f4bdfb9736457b7a57a2d055c4
Folaido <65743027+Folaido@users.noreply.github.com>
2021-05-17 18:43:52 -0500

    Update p4.py

--- p4.py
+++ p4.py
@@ -2,8 +2,15 @@ import os
 import pandas as pd
 from natsort import natsorted
 
+"""Count the total tone scores of the 7 tones
+    Args:
+        path: The path of the raw data (Covid NYT or Covid Guardian)
+    Returns:
+         An array contains the sum of every tone, listed in sequence: [Analytical, Sad, Confidence, Anger, Tentative, Fear, Joy] 
+    """
 
-def sum_total(path):
+
+def covid_count(path):
     res = [0, 0, 0, 0, 0, 0, 0]
     total = 0
     filter = 0
@@ -16,7 +23,7 @@ def sum_total(path):
                 for ind in data.index:
                     url = data["URL"][ind]
                     total += 1
-                    if not clean(url) and url not in se:
+                    if not cleanNYT(url) and url not in se:
                         filter += 1
                         se.add(url)
                         if data['Analy'][ind] > 0.5:
@@ -39,16 +46,39 @@ def sum_total(path):
     return res
 
 
-def NYTclean(url):
-    return (
-                       'coronavirus' not in url and 'covid' not in url and 'virus' not in url and 'pandemic' not in url and 'vaccine' not in url) or 'interactive' in url or 'photo' in url or 'video' in url or "podcast" in url or "quotation-of-the-day" in url
+"""Phrase 1 + Phrase 2 filtering for NYT articles
+    Args:
+        url: The url of the article
+    Returns:
+         True if the article will be filtered, False otherwise
+    """
 
 
-
-def clean(url):
+def cleanNYT(url):
     return ('coronavirus' not in url and 'covid' not in url and 'virus' not in url and 'pandemic' not in url
             and 'vaccine' not in url) or 'interactive' in url or "picture" in url or "photo" in url or "movie" in url \
            or "music" in url or "book" in url or "share" in url
+
+
+"""Phrase 1 + Phrase 2 filtering for Guardian articles
+    Args:
+        url: The url of the article
+    Returns:
+         True if the article will be filtered, False otherwise
+    """
+
+
+def cleanGuardian(url):
+    return ('coronavirus' not in url and 'covid' not in url and 'virus' not in url and 'pandemic' not in url
+            and 'vaccine' not in url) or 'interactive' in url or 'photo' in url or 'video' in url or "podcast" in url or "quotation-of-the-day" in url
+
+
+"""Count the total tone scores of the 7 tones
+    Args:
+        path: The path of the raw data (H1N1 NYT or H1N1 Guardian)
+    Returns:
+         An array contains the sum of every tone, listed in sequence: [Analytical, Sad, Confidence, Anger, Tentative, Fear, Joy] 
+    """
 
 
 def h1n1_count(path):
